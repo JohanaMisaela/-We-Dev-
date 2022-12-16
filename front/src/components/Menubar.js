bc86064dd2160c0f4385a5525b7d77bc434b9573
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2022-12-16 22:43:52 +0300

    navbar hover

--- front/src/components/Menubar.js
+++ front/src/components/Menubar.js
@@ -1,17 +1,72 @@
 import React from 'react'
 import { IonIcon } from '@ionic/react'
 import './../assets/styles/navbar.css'
-import { Link } from 'react-router-dom'
 import { homeOutline ,addOutline, personOutline,playOutline ,chatboxOutline} from 'ionicons/icons'
 function Menubar() {
   return (
     <div>
          <div>
-        <Link to='/Home'><IonIcon icon={homeOutline}></IonIcon></Link>
-        <Link to='/Add'><IonIcon icon={addOutline}></IonIcon></Link>
-        <Link to='/Profil'><IonIcon icon={personOutline}></IonIcon></Link>
-        <Link to='/Play'><IonIcon icon={playOutline}></IonIcon></Link>
-        <Link to='/Chat'><IonIcon icon={chatboxOutline}></IonIcon></Link>
+
+         <nav>
+      <ul>
+         <li>
+         <span>
+         <IonIcon  aria-hidden="true" icon={homeOutline} />
+            </span>
+            <span><a  href='/Home'>Home</a></span>
+            </li>
+            <li>
+         <span>
+         <IonIcon  aria-hidden="true" icon={addOutline} />
+            </span>
+            <span><a  href='/Add'>Add</a></span>
+            </li>
+            <li>
+         <span>
+         <IonIcon  aria-hidden="true" icon={personOutline} />
+            </span>
+            <span><a  href='/Profil'>Profil</a></span>
+            </li>
+            <li>
+         <span>
+         <IonIcon  aria-hidden="true" icon={playOutline} />
+            </span>
+            <span><a  href='/Play'>Play</a></span>
+            </li>
+            <li>
+         <span>
+         <IonIcon  aria-hidden="true" icon={chatboxOutline} />
+            </span>
+            <span><a  href='/Chat'>Chat</a></span>
+            </li>
+         </ul>
+      </nav>
+
+         {/* <a href='/Home'>
+            <span className='icon1'>
+            <IonIcon icon={homeOutline} />
+              </span>
+                </a>
+                <a href='/Add'>
+                <span className='icon1'>
+            <IonIcon icon={addOutline} />
+              </span>
+                </a>
+                <a href='/Profil'>
+            <span className='icon1'>
+            <IonIcon icon={personOutline} />
+              </span>
+                </a>
+                <a href='/Play'>
+                <span className='icon1'>
+            <IonIcon icon={playOutline} />
+              </span>
+                </a>
+                <a href='/Chat'>
+                <span className='icon1'>
+            <IonIcon icon={chatboxOutline} />
+              </span>
+                </a> */}
 
     </div>
 
