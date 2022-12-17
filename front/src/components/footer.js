57e8f9e32484099b598fc9ba10d5d9351d55daaa
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2022-12-17 10:35:08 +0300

    copyright

--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
   return (
     <div>
         <div class="col-md-6 col-lg-8">
             <p class="copyright">
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.</p>
+  Copyright &copy; {currentYear} </p>
+  <p class="copyright">All rights reserved. </p>
           </div>
     </div>
   )
